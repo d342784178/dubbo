f4fc8a568e48ccdc429f29c6674512bd66b57c21
董良剑 <dongliangjian@qccr.com>
2017-08-29 10:46:39 +0800

    ~

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -32,12 +32,13 @@ import com.alibaba.dubbo.rpc.Invoker;
 /**
  * ConsistentHashLoadBalance
  * ketama一致性hash
- * 
+ *
  * @author william.liangf
  */
 public class ConsistentHashLoadBalance extends AbstractLoadBalance {
 
-    private final ConcurrentMap<String, ConsistentHashSelector<?>> selectors = new ConcurrentHashMap<String, ConsistentHashSelector<?>>();
+    private final ConcurrentMap<String, ConsistentHashSelector<?>> selectors = new ConcurrentHashMap<String,
+            ConsistentHashSelector<?>>();
 
     @SuppressWarnings("unchecked")
     @Override
@@ -56,20 +57,22 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
 
         private final TreeMap<Long, Invoker<T>> virtualInvokers;//虚拟节点集合,根据key排序 均匀分布
 
-        private final int                       replicaNumber;//每个服务的虚拟节点数量
-        
-        private final int                       identityHashCode;//对象的hash码
-        
-        private final int[]                     argumentIndex;
+        private final int replicaNumber;//每个服务的虚拟节点数量
+
+        private final int identityHashCode;//对象的hash码
+
+        private final int[] argumentIndex;
 
         public ConsistentHashSelector(List<Invoker<T>> invokers, String methodName, int identityHashCode) {
             this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
             this.identityHashCode = System.identityHashCode(invokers);
             URL url = invokers.get(0).getUrl();
             this.replicaNumber = url.getMethodParameter(methodName, "hash.nodes", 160);
-            String[] index = Constants.COMMA_SPLIT_PATTERN.split(url.getMethodParameter(methodName, "hash.arguments", "0"));
+            //缺省只对第一个参数Hash，如果要修改，请配置<dubbo:parameter key="hash.arguments" value="0,1"
+            String[] index = Constants.COMMA_SPLIT_PATTERN.split(url.getMethodParameter(methodName, "hash.arguments",
+                    "0"));
             argumentIndex = new int[index.length];
-            for (int i = 0; i < index.length; i ++) {
+            for (int i = 0; i < index.length; i++) {
                 argumentIndex[i] = Integer.parseInt(index[i]);
             }
             for (Invoker<T> invoker : invokers) {
@@ -88,7 +91,7 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         }
 
         public Invoker<T> select(Invocation invocation) {
-            //取出参数
+            //取出需要进行hash的参数
             String key = toKey(invocation.getArguments());
             //对参数md5
             byte[] digest = md5(key);
@@ -125,8 +128,8 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         private long hash(byte[] digest, int number) {
             return (((long) (digest[3 + number * 4] & 0xFF) << 24)
                     | ((long) (digest[2 + number * 4] & 0xFF) << 16)
-                    | ((long) (digest[1 + number * 4] & 0xFF) << 8) 
-                    | (digest[0 + number * 4] & 0xFF)) 
+                    | ((long) (digest[1 + number * 4] & 0xFF) << 8)
+                    | (digest[0 + number * 4] & 0xFF))
                     & 0xFFFFFFFFL;
         }
 

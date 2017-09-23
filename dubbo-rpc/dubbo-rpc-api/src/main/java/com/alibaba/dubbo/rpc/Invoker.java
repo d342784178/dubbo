966837547b5545e1dc003a3950c2e2503bd6795a
董良剑 <dongliangjian@qccr.com>
2017-09-23 16:27:59 +0800

    ~

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/Invoker.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/Invoker.java
@@ -19,7 +19,6 @@ import com.alibaba.dubbo.common.Node;
 
 /**
  * Invoker. (API/SPI, Prototype, ThreadSafe)
- * 负责ServiceIntf的具体实现
  * 分三种:
  * 1.AbstractInvoker      通过网络调用远程服务 (客户端用)
  * 2.AbstractProxyInvoker 调用本地实现 (服务端用)
